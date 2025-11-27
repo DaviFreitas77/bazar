d0adb7bbe540f97d6820f27724ffbb60b7e71403
Davi Freitas <davifreitaz999@gmail.com>
2025-11-27 01:55:54 -0300

    alterando config axios para enviar o token

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://web-production-72b71.up.railway.app/api",
-  withCredentials: true, 
-  xsrfCookieName: 'XSRF-TOKEN', 
+  withCredentials: true,
+  withXSRFToken: true,
+   xsrfCookieName: 'XSRF-TOKEN', 
   xsrfHeaderName: 'X-XSRF-TOKEN',
-  
 });
 
 export const api = instance;
