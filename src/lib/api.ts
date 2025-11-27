f86f6ece495e72b36632133ed9c33735ddda69ae
Davi Freitas <davifreitaz999@gmail.com>
2025-11-27 00:49:24 -0300

    base url production

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://web-production-72b71.up.railway.app/api",
+  baseURL: "https://web-production-72b71.up.railway.app/api",
   withCredentials: true,
   withXSRFToken: true,
 });
