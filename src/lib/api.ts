2e8d21c809bf4e278a98500dab4642e382c6afef
Davi Freitas <davifreitaz999@gmail.com>
2025-11-26 21:08:22 -0300

    config api 1

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://2c511dad9198.ngrok-free.app/api",
+  baseURL: "http://localhost:8000/api",
   withCredentials: true,
   withXSRFToken: true,
 });
