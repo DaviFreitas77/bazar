279126a9fa3f8adfd2484b29f80d8185c12e08d6
Davi Freitas <davifreitaz999@gmail.com>
2025-11-27 01:43:04 -0300

    alterando config do axios

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://web-production-72b71.up.railway.app/api",
-  withCredentials: true,
-  withXSRFToken: true,
+  withCredentials: true, 
+  xsrfCookieName: 'XSRF-TOKEN', 
+  xsrfHeaderName: 'X-XSRF-TOKEN',
+  
 });
 
 export const api = instance;
