c001ce7815d1aabc582cb998a3a207610d11eb28
Davi Freitas <davifreitaz999@gmail.com>
2026-01-14 19:34:14 -0300

    config vite para produção

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+   preview: {
+    host: '0.0.0.0', 
+    port: 4173,      
+    allowedHosts: true 
+  },
+  
 })
