5cf1cbac43fd489e54aab82bcb353031802ec206
Davi Freitas <davifreitaz999@gmail.com>
2026-01-14 23:38:21 -0300

    usar base url global nas requisições de auth

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,47 +1,39 @@
 import type { login } from "@/components/auth/types/login";
 import type { Register } from "@/components/auth/types/register";
+import { api } from "@/lib/api";
 import type { AxiosResponse } from "axios";
 import axios from "axios";
 
 export const ensureCsrf = async () => {
-  await axios.get("https://web-production-edc6.up.railway.app/sanctum/csrf-cookie", {
+  await axios.get("http://localhost:8000/sanctum/csrf-cookie", {
     withCredentials: true,
-    withXSRFToken: true,                                                                                                                                                                                                                                                                                                                                        
+    withXSRFToken: true,                                                                                                                                                                                                                                                                                                                                     
   });
 };
 
 export const getMe = async () => {
-  const response = await axios.get("https://web-production-edc6.up.railway.app/auth/profile", {
-    withCredentials: true,
-    withXSRFToken: true,
-  });
+  const response = await api.get("/auth/profile");
   return response.data;
 };
 export const registerUser = async (data: Register) => {
-  const response: AxiosResponse = await axios.post("https://web-production-edc6.up.railway.app/auth/register", data, {
-    withCredentials: true,
-    withXSRFToken: true,
-  });
+  const response: AxiosResponse = await api.post("/auth/register", data);
   return response.data;
 };
 
 export const loginUser = async (data: login) => {
-  const response: AxiosResponse = await axios.post("https://web-production-edc6.up.railway.app/auth/login", data, {
-    withCredentials: true,
-    withXSRFToken: true,
-  });
+  const response: AxiosResponse = await api.post("/auth/login", data);
   return response.data;
 };
 
 export const logout = async () => {
-  const response: AxiosResponse = await axios.post(
-    "https://web-production-edc6.up.railway.app/auth/logout",
+  const response: AxiosResponse = await api.post(
+    "/auth/logout",
     {},
     {
-      withCredentials: true,
-      withXSRFToken: true,
+
     }
   );
 
   return response;
 };
+
