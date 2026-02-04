0a51b2d9b494d0642ca45a25a3e4b809838cf734
Davi Freitas <davifreitaz999@gmail.com>
2026-02-04 18:56:36 -0300

    add animação nas métricas

--- client/src/components/admin/layout/metrics.tsx
+++ client/src/components/admin/layout/metrics.tsx
@@ -1,7 +1,8 @@
 import CountUp from "@/components/styles/countUp";
 import type { ReactNode } from "react";
 import { motion } from "framer-motion";
-
+import { BorderBeam } from "@/components/ui/border-beam";
+import ShinyText from "@/components/styles/shineText";
 
 interface MetricItem {
   title: string;
@@ -21,14 +22,22 @@ interface MetricsProps {
 export function Metrics({ items }: MetricsProps) {
   return (
     <section className="w-full py-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sha">
+      <div className="relative  w-full overflow-hidden grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sha">
         {items.map((item, index) => (
-          <motion.div animate={{ opacity: 1,y:0 }} initial={{ opacity: 0, y: -50 }}transition={{
-            duration:0.8,
-            delay:0.2,
-            ease: "anticipate",
-          }} key={index} className={`flex flex-col gap-4 px-4 border border-gray-200 rounded-lg bg-primary-200 h-40  justify-center ${item.styleCard && item.styleCard}`}>
-            <span className={`${item.titleStyle ?? "text-gray-800 font-semibold text-sm"}`}>{item.title}</span>
+          <motion.div
+            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -50 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.2,
+              ease: "anticipate",
+            }}
+            key={index}
+            className={`relative overflow-hidden flex flex-col gap-4 px-4 border border-gray-200 rounded-lg bg-primary-200 h-40 justify-center ${item.styleCard ?? ""}`}
+          >
+            <BorderBeam size={50} duration={10} colorFrom="#cccc" colorTo="#ccc" delay={3} />
+
+            <ShinyText text={`${item.title}`} speed={2} delay={3} color="#000" shineColor="#ffffff" spread={120} direction="left" yoyo={false} pauseOnHover={false} disabled={false} />
 
             <span className={`${item.valueStyle ?? "text-gray-700 font-semibold text-3xl"}`}>
               {item.prefix ? <CountUp from={0} to={item.value} separator="," direction="up" duration={1} className="count-up-text" prefix={item.prefix} /> : <CountUp from={0} to={item.value} separator="," direction="up" duration={1} className="count-up-text" />}
