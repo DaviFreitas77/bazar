53c23580200e3de1397e18bcbac2a09daacefb2d
Davi Freitas <davifreitaz999@gmail.com>
2026-02-04 19:55:52 -0300

    permitir url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path" 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),],
-    server: {
-    host: '0.0.0.0', 
+  plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    port: 5173,
+    allowedHosts: ["fabulous-light-production-ef57.up.railway.app"],
+  },
+  server: {
+    host: "0.0.0.0",
     port: 5173,
     watch: {
-      usePolling: true, 
-      interval: 100,    
+      usePolling: true,
+      interval: 100,
     },
   },
 
-    resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    
   },
-   
-  
-})
+});
