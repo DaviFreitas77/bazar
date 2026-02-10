a2012b8744fef190df223c20ecf24f6b134f14c4
Davi Freitas <davifreitaz999@gmail.com>
2026-02-09 22:03:37 -0300

    config webhook pt2

--- server/app/Http/Controllers/Webhook/McphookController.php
+++ server/app/Http/Controllers/Webhook/McphookController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Webhook;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use MercadoPago\MercadoPagoConfig;
+
 
 class McphookController extends Controller
 {
@@ -16,11 +19,17 @@ class McphookController extends Controller
 
         Log::info('Webhook Mercado Pago:', $request->all());
 
-  
+
         $paymentId = $request->data['id'] ?? null;
 
         if ($paymentId) {
-           
+            $response = Http::get("https://api.mercadopago.com/v1/payments/{$paymentId}", [
+                'access_token' => env('MERCADO_PAGO_ACCESS_TOKEN'),
+            ]);
+
+            $data = $response->json();
+
+            Log::info('Payment Details:', $data);
         }
         return response()->json(['status' => 'success'], 200);
     }
