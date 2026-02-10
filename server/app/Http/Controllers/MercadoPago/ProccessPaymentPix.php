14fc0c8eb7007accee7e69d155c62a6aa2111fdd
Davi Freitas <davifreitaz999@gmail.com>
2026-02-10 01:46:22 -0300

    remove identifcação test

--- server/app/Http/Controllers/MercadoPago/ProccessPaymentPix.php
+++ server/app/Http/Controllers/MercadoPago/ProccessPaymentPix.php
@@ -38,10 +38,7 @@ class ProccessPaymentPix extends Controller
                     "email" => $data['payer']['email'],
                     "first_name" => $data['payer']['first_name'] ?? '',
                     "last_name" => $data['payer']['last_name'] ?? '',
-                    "identification" => [
-                        "type" => "CPF",
-                        "number" => "19119119100"
-                    ]
+                   
 
                 ],
                 "external_reference" => strval($order)
