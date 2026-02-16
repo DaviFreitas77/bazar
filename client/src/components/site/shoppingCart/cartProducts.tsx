3fa8976d06994001da4623f0e3eb85d72340dc65
Davi Freitas <davifreitaz999@gmail.com>
2026-02-16 18:45:29 -0300

    style: add carregamento ao incrementar produto

--- client/src/components/site/shoppingCart/cartProducts.tsx
+++ client/src/components/site/shoppingCart/cartProducts.tsx
@@ -10,40 +10,47 @@ import { apiAddProduct } from "@/api/site/shoppingCart.api";
 import { toast } from "sonner";
 
 import { SkeletonRecomendedProducts } from "./skeletonRecomendedProduct";
+import { useState } from "react";
+import { Loading } from "../loading/loading";
 
 export function CartProducts() {
   const { state, dispatch } = useCart();
   const { data: products } = useAllProducts();
   const navigate = useNavigate();
   const { setOpenCart } = useUI();
+  const [loadingId, setLoadingId] = useState<string | number | null>(null);
 
   const limited = products?.slice(0, 3);
 
- const handleIncrement = async (item: CartItem) => {
-  try {
-    await apiAddProduct({
-      id: item.id,
-      name: item.name,
-      price: Number(item.price),
-      image: item.image,
-      quantity: 1,
-      color: item.color,
-      size: item.size,
-    });
+  const handleIncrement = async (item: CartItem) => {
+    const uniqueId = `${item.id}-${item.color}-${item.size}`;
 
-    dispatch({
-      type: "increment",
-      payload: { id: item.id, color: item.color, size: item.size }
-    });
+    setLoadingId(uniqueId); 
+    try {
+      await apiAddProduct({
+        id: item.id,
+        name: item.name,
+        price: Number(item.price),
+        image: item.image,
+        quantity: 1,
+        color: item.color,
+        size: item.size,
+      });
 
-  } catch (error: any) {
-    if (error.response?.status === 400) {
-      toast.error(error.response.data.message);
-    } else {
-      toast.error("Erro ao adicionar produto");
+      dispatch({
+        type: "increment",
+        payload: { id: item.id, color: item.color, size: item.size },
+      });
+    } catch (error: any) {
+      if (error.response?.status === 400) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("Erro ao adicionar produto");
+      }
+    } finally {
+      setLoadingId(null);
     }
-  }
-};
+  };
 
   const handleDecrement = (item: CartItem) => {
     dispatch({ type: "decrement", payload: { id: item.id, color: item.color, size: item.size } });
@@ -51,33 +58,37 @@ export function CartProducts() {
   return (
     <section className="mt-2 flex flex-col gap-2">
       {state.length > 0 ? (
-        state.map((item, index) => (
-          <div key={index} className="flex gap-3 border-b pb-3 border-gray-200">
-            <img src={item.image} alt={item.name} className="w-20 object-cover" />
-            <div className="w-full">
-              <p className="font-semibold text-gray-700">{item.name}</p>
-              <p className="text-xs text-gray-400">Quantidade: {item.quantity}</p>
-              <p className="text-xs text-gray-400">Cor: {item.colorName}</p>
-              <p className="text-xs text-gray-400">Tamanho: {item.sizeName}</p>
+        state.map((item, index) => {
+          const uniqueId = `${item.id}-${item.color}-${item.size}`;
+          return (
+            <div key={index} className="flex gap-3 border-b pb-3 border-gray-200">
+              <img src={item.image} alt={item.name} className="w-20 object-cover" />
+              <div className="w-full">
+                <p className="font-semibold text-gray-700">{item.name}</p>
+                <p className="text-xs text-gray-400">Quantidade: {item.quantity}</p>
+                <p className="text-xs text-gray-400">Cor: {item.colorName}</p>
+                <p className="text-xs text-gray-400">Tamanho: {item.sizeName}</p>
 
-              <div className="flex items-end justify-between">
-                <p className="font-semibold text-gray-900 mt-10">{Number(item.price).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
+                <div className="flex items-end justify-between">
+                  <p className="font-semibold text-gray-900 mt-10">{Number(item.price).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
 
-                <div className="flex items-center gap-3 mt-1">
-                  <button onClick={() => handleDecrement(item)} className="p-1 rounded-full border border-primary-50 hover:bg-primary-100 hover:text-white cursor-pointer">
-                    <Minus size={16} />
-                  </button>
+                  <div className="flex items-center gap-3 mt-1">
+                    <button onClick={() => handleDecrement(item)} className="p-1 rounded-full border border-primary-50 hover:bg-primary-100 hover:text-white cursor-pointer">
+                      <Minus size={16} />
+                    </button>
 
-                  <p className="text-sm font-medium text-gray-700">{item.quantity}</p>
+                    <p className="text-sm font-medium text-gray-700">{item.quantity}</p>
 
-                  <button onClick={() => handleIncrement(item)} className="p-1 border rounded-full border-primary-50 hover:bg-primary-100 hover:text-white cursor-pointer">
-                    <Plus size={16} />
-                  </button>
+                    <button onClick={() => handleIncrement(item)} className="p-1 border rounded-full border-primary-50 hover:bg-primary-100 hover:text-white cursor-pointer">
+                      {/* Verifica se o ID carregando é igual ao ID deste botão */}
+                      {loadingId === uniqueId ? <Loading  width={16} height={16}/> : <Plus size={16} />}
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))
+          );
+        })
       ) : (
         <div className="text-gray-600 flex flex-col justify-center items-center gap-2 mt-4">
           <TbMoodConfuzed size={40} />
