e16f3bf66cf84f08afac7a927ef9439b9ef0ef14
Davi Freitas <davifreitaz999@gmail.com>
2026-02-16 17:22:40 -0300

    fix: dockerfile

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://bazar-production-4a07.up.railway.app/api",
+  baseURL: "http://localhost:8000/api",
 });
 
 export const api = instance;
