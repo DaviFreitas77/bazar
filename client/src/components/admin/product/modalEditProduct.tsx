0a72d3f365e6830d4272319e450d2fe09ea6b495
Davifreitas77 <davifreitaz999@gmail.com>
2026-02-25 21:13:33 -0300

    fix:style

--- client/src/components/admin/product/modalEditProduct.tsx
+++ client/src/components/admin/product/modalEditProduct.tsx
@@ -106,7 +106,7 @@ export function ModalEditProduct({ productId, onClose }: ModalEditProductProps) 
                   Cancelar
                 </button>
 
-                <button onClick={editProduct} className="px-4 py-2 text-sm rounded-md bg-primary-50 text-white hover:opacity-90">
+                <button onClick={editProduct} className="px-4 py-2 text-sm rounded-md bg-primary-50 text-white hover:opacity-90 cursor-pointer">
                   Salvar alterações
                 </button>
               </div>
