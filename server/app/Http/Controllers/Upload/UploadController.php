59453a5df912a0cfb6b23c1316a875d07988fee6
Davifreitas77 <davifreitaz999@gmail.com>
2026-02-25 22:13:54 -0300

    fix:tamanho imagem

--- server/app/Http/Controllers/Upload/UploadController.php
+++ server/app/Http/Controllers/Upload/UploadController.php
@@ -15,7 +15,7 @@ class UploadController extends Controller
     public function __invoke(Request $request)
     {
         $request->validate([
-            'image' => 'required|image|mimes:jpg,jpeg,png,webp|max:2048',
+            'image' => 'required|image|mimes:jpg,jpeg,png,webp',
         ]);
 
         $path = $request->file('image')->store('products', 'public');
