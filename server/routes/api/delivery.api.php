ae5edf220622949cbff959d981c70fa0db69e708
Davifreitas77 <davifreitaz999@gmail.com>
2026-03-09 21:59:20 -0300

    feat: config delivery pt/3

--- server/routes/api/delivery.api.php
+++ server/routes/api/delivery.api.php
@@ -2,10 +2,9 @@
 
 use App\Http\Controllers\Delivery\AuthDelivery;
 use App\Http\Controllers\Delivery\CalculateFreteController;
-use App\Http\Controllers\Delivery\MelhorEnvioController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('delivery')->group(function () {
-    Route::post('callback', AuthDelivery::class);
+    Route::get('callback', AuthDelivery::class);
     Route::post('calculate-frete',CalculateFreteController::class);
 });
