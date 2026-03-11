edb82e0bde81a7227d226dbf9f08252bd82889cf
Davifreitas77 <davifreitaz999@gmail.com>
2026-03-11 02:13:52 -0300

    coment filter and show all services

--- server/app/Http/Controllers/Delivery/CalculateFreteController.php
+++ server/app/Http/Controllers/Delivery/CalculateFreteController.php
@@ -53,13 +53,13 @@ class CalculateFreteController extends Controller
                 $data = $response->json();
 
                 Log::info("Frete calculado", $data);
-                $filterServices = array_filter(
-                    $data,
-                    fn($service) =>
-                    in_array($service['name'], ['PAC', 'SEDEX'])
-                );
+                // $filterServices = array_filter(
+                //     $data,
+                //     fn($service) =>
+                //     in_array($service['name'], ['PAC', 'SEDEX'])
+                // );
 
-                return response()->json($filterServices);
+                return response()->json($data);
             } else {
                 $data = $response->json();
                 Log::error("Erro ao calcular frete", $data);
