d5ad4c1f65e327f797dee9c20bea1cb1210467fd
Davifreitas77 <davifreitaz999@gmail.com>
2026-03-20 21:10:11 -0300

    fix

--- client/src/pages/site/politicPrivacity/politicPrivacity.tsx
+++ client/src/pages/site/politicPrivacity/politicPrivacity.tsx
@@ -1,4 +1,4 @@
-import { NavigationInformation } from "@/components/site/InfoNav/navigationInformations";
+import { NavigationInformation } from "@/components/site/infoNav/NavigationInformations";
 
 export function PoliticPrivacity() {
 
